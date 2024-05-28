747d8c0edbe16a2798707d8f888ac60183eedbd5
Marcigdd <marcigdd@gmail.com>
2024-05-28 23:22:25 +0200

    feat: wip

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,29 +2,134 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import path = require('path');
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 3, // Default is all AZs in the region
+      natGateways: 0, // Default is 1 NAT gateway per AZ
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC, // Configure subnet as public
+        },
+        {
+          cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT, // Configure subnet as private with NAT
+        },
+      ],
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc, // Associate the security group with the VPC
+      description: 'Allow traffic to RDS from Lambda function',
+      allowAllOutbound: true, // Allow outbound traffic by default
+    });
+
+    // Define the RDS instance
+    const dbInstance = new rds.DatabaseInstance(this, 'DBInstance', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroups: [securityGroup],
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_16_2,
+      }),
+      allocatedStorage: 20,
+      backupRetention: cdk.Duration.days(0),
+      deletionProtection: false,
+      maxAllocatedStorage: 100,
+      multiAz: false,
+      publiclyAccessible: true, // Make it publicly accessible for simplicity
+      storageType: rds.StorageType.GP2,
+      databaseName: 'mydb',
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO,
+      ),
+    });
+
     // Create a Lambda function with Node.js and your Nest.js application
     const nestJsFunction = new lambda.Function(this, 'NestJsFunction', {
-      code: lambda.Code.fromAsset(
-        path.resolve(__dirname, '../cart-api.zip'),
-      ),
+      vpc, // Assign the VPC to the Lambda funct
+      allowPublicSubnet: true,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_NAT },
+      securityGroups: [securityGroup],
+      environment: {
+        SECRET_ARN: dbInstance.secret?.secretArn || '',
+      },
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '../cart-api.zip')),
       handler: 'dist/src/lambda.handler', // The exported handler in your entry point file
       runtime: lambda.Runtime.NODEJS_20_X,
       logRetention: cdk.aws_logs.RetentionDays.FIVE_DAYS,
       memorySize: 1256,
-      timeout: cdk.Duration.seconds(10),
+      timeout: cdk.Duration.seconds(5),
     });
 
+    nestJsFunction.role?.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['secretsmanager:GetSecretValue'],
+        resources: [dbInstance.secret?.secretArn || ''],
+      }),
+    );
+
+    // Create a VPC endpoint for Secrets Manager
+    const secretManagerEndpoint = new ec2.InterfaceVpcEndpoint(
+      this,
+      'SecretsManagerEndpoint',
+      {
+        service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+        vpc,
+        subnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_NAT },
+      },
+    );
+
+    // Update the Lambda function's security group to allow outbound traffic to Secrets Manager
+    const lambdaSecurityGroup = nestJsFunction.connections.securityGroups[0];
+    secretManagerEndpoint.connections.allowFrom(
+      lambdaSecurityGroup,
+      ec2.Port.tcp(443),
+    );
+
+    const lambdaRole = new iam.Role(this, 'LambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    // Attach an inline policy granting access to Secrets Manager
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['secretsmanager:GetSecretValue'],
+        resources: ['*'], // Adjust resource ARN as needed
+      }),
+    );
+    // Assign the IAM role to the Lambda function
+    nestJsFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['secretsmanager:GetSecretValue'],
+        resources: ['*'], // Adjust resource ARN as needed
+      }),
+    );
+
+    // Create the API Gateway
     const api = new apigw.LambdaRestApi(this, 'rest-api-gateway', {
       handler: nestJsFunction,
       restApiName: 'NestJsApiGateway',
       proxy: true,
       deploy: true,
     });
+
+    // Create the API Gateway
   }
 }
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack');
+app.synth();
--- src/lambda.ts
+++ src/lambda.ts
@@ -9,6 +9,8 @@ import { NestFactory } from '@nestjs/core';
 import { eventContext } from 'aws-serverless-express/middleware';
 import * as helmet from 'helmet';
 import { createServer, proxy } from 'aws-serverless-express';
+import { Client } from 'pg';
+import * as AWS from 'aws-sdk';
 
 const verbose = debug('api:verbose handler');
 let cachedServer: Server;
@@ -33,11 +35,73 @@ const bootstrap = async () => {
   return cachedServer;
 };
 
+const setDBPasswordFromSecretsManager = async () => {
+  console.log(AWS.SecretsManager);
+  console.log('Secret ARN: ', process.env.SECRET_ARN);
+  const secretsManager = new AWS.SecretsManager({
+    region: 'us-east-2', // Replace with your AWS region
+  });
+  console.log('secretsManageer'); // Create Secrets Manager instance
+  const dbSecretArn = process.env.SECRET_ARN;
+
+  if (dbSecretArn) {
+    // Fetch secret value from Secrets Manager
+    console.time('getSecretValue');
+    const data = await secretsManager
+      .getSecretValue({ SecretId: dbSecretArn })
+      .promise();
+    console.timeEnd('getSecretValue');
+    const secret = JSON.parse(data.SecretString || '');
+    console.log('Secret: ', secret);
+    const dbPassword = secret.password;
+    process.env.DB_PASSWORD = dbPassword;
+    return secret; // Set DB password as environment variable
+  } else {
+    console.error('DB_SECRET_ARN environment variable not set.');
+  }
+};
+
 export async function handler(event: APIGatewayProxyEvent, context: Context) {
   verbose('Event: %j', event);
   verbose('Context: %j', context);
   console.log('Event: ', event);
   console.log('Context: ', context);
+  console.log('Secret: ', process.env.SECRET_ARN);
+  let secret: any;
+  try {
+    secret = await setDBPasswordFromSecretsManager();
+  } catch (error) {
+    console.error('Error setting DB password from Secrets Manager: ', error);
+  }
+  // console.log('SecretsManager: ', secretsManager);
+  // const secretData = await secretsManager
+  //   .getSecretValue({
+  //     SecretId: process.env.SECRET_ARN,
+  //   })
+  //   .promise();
+  // const secret = JSON.parse(secretData.SecretString);
+  console.log('trying to connect to pg client: ');
+  const client = new Client({
+    host: secret.host,
+    port: secret.port,
+    database: secret.dbname,
+    user: secret.username,
+    password: secret.password,
+  });
+  console.log('client: ', client);
+
+  await client.connect();
+
+  console.log('connected to pg client: ');
+
+  // Run your queries here, for example:
+  const res = await client.query('SELECT NOW()');
+  console.log(res.rows[0].now);
+
+  await client.end();
+  console.log('Secret: ', secret);
+
+  console.log('Result: ', res);
   cachedServer = await bootstrap();
   return proxy(cachedServer, event, context, 'PROMISE').promise;
 }
